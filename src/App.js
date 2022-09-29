b0b6f0a93acf72ed22bb9fba0d554eea2d041f26
Abhilash Nair <digilabcloudhub@gmail.com>
2022-09-29 13:34:02 +0100

    changes in notes app

--- src/App.js
+++ src/App.js
@@ -1,101 +1,44 @@
 import './App.css';
 import {nanoid} from 'nanoid';
-import AddNote from './components/AddNote';
-import Note from './components/Note';
-import {useEffect, useState,useMemo} from 'react';
-import SearchNote from './components/SearchNote';
+import {useState} from 'react';
+import NotesList from './components/NotesList';
 import Header from './components/Header';
+
+
 const App =()=>{
-const initialData=[
-  {
-    id:nanoid(),
-    text:'This is my first Note',
-    date:'15/04/2022'
-  },
-  {
-    id:nanoid(),
-    text:'This is my second Note',
-    date:'15/04/2022'
-  },
-  {
-    id:nanoid(),
-    text:'This is my third Note',
-    date:'15/04/2022'
-  },
-  {
-    id:nanoid(),
-    text:'This is my fourth Note',
-    date:'15/04/2022'
-  }
-]
-
-const [notes,setNotes]=useState(initialData);
-const [searchText,setSearchText]=useState('');
-const [darkMode,setDarkmode]=useState(false);
-
-  useEffect(()=>{
-  const savedNotes=JSON.parse(localStorage.getItem('react-notes-app-data'));
-  if(savedNotes){
-    
-  setNotes(savedNotes);}
-},[]); 
- 
-
-
- useEffect (()=>{
+  const [darkMode,setDarkmode]=useState(false);
   
-    localStorage.setItem('react-notes-app-data',JSON.stringify(notes));
+  
  
-},[notes]);
-
-
-const addNote =(text)=>{
-  const date=new Date();
-  const data=text;
-  const newNote=
+  const initialData=[
     {
       id:nanoid(),
-      text:data,
-      date:date.toLocaleDateString()
+      text:'This is my first Note',
+      date:'15/04/2022'
+    },
+    {
+      id:nanoid(),
+      text:'This is my second Note',
+      date:'15/04/2022'
+    },
+    {
+      id:nanoid(),
+      text:'This is my third Note',
+      date:'15/04/2022'
+    },
+    {
+      id:nanoid(),
+      text:'This is my fourth Note',
+      date:'15/04/2022'
     }
-     setNotes(prevNotes => [...prevNotes,newNote]) ;
-     
-  }
-
-  const deleteNote =(id)=>{
-        const deleteNotes=notes.filter((item)=>item.id!==id);
-        setNotes(deleteNotes);
-  }
-
-
-
-  const filteredNotes = useMemo(() => {
-  if (!searchText) {
-    return notes;
-  }
-  return notes.filter(note=> note.text.toLowerCase().includes(searchText));
+  ]
   
-  }, [searchText, notes]);
-
   return(
     <div  className={` ${darkMode ? 'dark-mode' : ''}`}>
     <div className='container '>
-      <Header toggle={setDarkmode}/>
-       <SearchNote handleSearchText={setSearchText}/>
-       
-      <div className="notes-list">
-       
-         {
-        filteredNotes.map((note)=>
-        <Note key={note.id} id={note.id} text={note.text} date={note.date} 
-        onDelete={()=>deleteNote(note.id)}/>
-        
-        )
-        
-      } 
-      
-      <AddNote onAdd={addNote}/>
-      </div>
+    <Header toggle={setDarkmode}/>
+    <NotesList initialData={initialData}/>
+    
     </div>
     </div>
   )
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,16 +1,83 @@
 
-import Note from "./Note";
+import {useEffect, useState,useMemo} from 'react';
+import {nanoid} from 'nanoid';
+import SearchNote from './SearchNote'
+import Note from './Note'
+import AddNote from './AddNote';
 
-const NotesList = ({note,addNote}) => {
-  return (
+const NotesList = ({initialData}) => {
+  const [notes,setNotes]=useState(initialData);
+  const [searchText,setSearchText]=useState('');
+  
+    useEffect(()=>{
+    const savedNotes=JSON.parse(localStorage.getItem('react-notes-app-data'));
+    if(savedNotes){
+      
+    setNotes(savedNotes);}
+  },[]); 
+   
+  
+  
+   useEffect (()=>{
     
-    <div className="notes-list">
-        {
-            note.map((notes)=><Note  id={notes.id} text={notes.text} date={notes.date}/>)
-        }
+      localStorage.setItem('react-notes-app-data',JSON.stringify(notes));
+   
+  },[notes]);
+  
+  
+  const addNote =(text)=>{
+    const date=new Date();
+    const data=text;
+    const newNote=
+      {
+        id:nanoid(),
+        text:data,
+        date:date.toLocaleDateString()
+      }
+       setNotes(prevNotes => [...prevNotes,newNote]) ;
        
-    </div>
-  )
+    }
+  
+    const deleteNote =(id)=>{
+          const deleteNotes=notes.filter((item)=>item.id!==id);
+          setNotes(deleteNotes);
+    }
+  
+  
+  
+    const filteredNotes = useMemo(() => {
+    if (!searchText) {
+      return notes;
+    }
+    return notes.filter(note=> note.text.toLowerCase().includes(searchText));
+    
+    }, [searchText, notes]);
+  
+    return(
+     
+        <>
+        <SearchNote handleSearchText={setSearchText}/>
+         
+        <div className="notes-list">
+         
+           {
+          filteredNotes.map((note)=>
+          <Note key={note.id} id={note.id} text={note.text} date={note.date} 
+          onDelete={()=>deleteNote(note.id)}/>
+          
+          )
+          
+        } 
+        
+        <AddNote onAdd={addNote}/>
+      </div>
+     
+        
+        
+        </>
+    )
+  
 }
 
 export default NotesList
+
